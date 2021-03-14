be3eafdb8142f22f313a473e39791e9a0aa40bfa
Renaud Gaucher <shiruno19@gmail.com>
2021-03-15 00:40:27 +0100

    push pull

--- js/code.js
+++ js/code.js
@@ -11,15 +11,15 @@ $(document).ready(function(){
     $('.range').on('input', function() {
                     var $set = $(".range").val();
                     $("#disp_nb_personne").html("Nombre de personnes :  "+ $set );
-                    
-                    $.post("regle3.php",{id_recette:$("#recette").val(),nb_personne:$set},function(rep){
-                        $("li_ingredient").html(rep);
+                   
+                    $.post("scripts/regle3.php",{id_recette:$("#recette").val(),nb_personne:$set},function(rep){
+                        $("#li_ingredient").html(rep);
+                        
                     });
     });
     $("#btn_create_ingredient").click(function(){
-        
         var nvlIngredient = $("#create_ingredient").val();
-        $.post("scripts/newIngredient.php",{nom_ingredient:nvlIngredient},function(rep){
+        $.post("scripts/newIngredient.php",{nom_ingredient:nvlIngredient},function(){
             console.log(nvlIngredient);
         });
     });
--- scripts/regle3.php
+++ scripts/regle3.php
@@ -2,9 +2,16 @@
 
 require "../class/Database.php";
 require "../class/Ingredient.php";
+require "../class/Recette.php";
 
 $dbh = Database::connect();
 
+if(!isset($_POST['id_recette'])){
+    return "is_recette";
+}
+if(!isset($_POST['nb_personne'])){
+    return "nb_personne";
+}
 $id_recette = $_POST['id_recette'];
 
 $nb_personne = $_POST['nb_personne'];
@@ -12,14 +19,13 @@ $nb_personne = $_POST['nb_personne'];
 if (!ctype_digit($id_recette) || !ctype_digit($nb_personne)) return false;
 else{
     $recette = Recette::getRecette($dbh, $id_recette);
-    $nb_personne_theorique = $recette['nb_personne'];
+    $nb_personne_theorique = $recette->nb_personne;
     if ($recette === null) {
         return false;
     }
     foreach ($recette->liste_ingredient as $ingredient) {
-        echo "<li>".($ingredient->quantite *$nb_personne/$nb_personne_theorique)."$ingredient->unite " . Ingredient::id_to_nomIngredient($dbh, $ingredient->id_ingredient)['nom'] . "</li>";
+        echo "<li>".($ingredient->quantite *$nb_personne/$nb_personne_theorique)." $ingredient->unite " . Ingredient::id_to_nomIngredient($dbh, $ingredient->id_ingredient)['nom'] . "</li>";
     }
     $dbh=null;
-    return $res;
 }
 
