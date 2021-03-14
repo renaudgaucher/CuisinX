c194a10539aa425f9729935d8418b17483708aaf
Renaud Gaucher <shiruno19@gmail.com>
2021-03-14 16:47:31 +0100

    modification class ingredient pour mise en coherance DB

--- class/Ingredient.php
+++ class/Ingredient.php
@@ -6,6 +6,38 @@ class Ingredient{
     public $quantite;
     public $unite;
     
+    public static function convert_id_to_nomIngredient($dbh,$id_ingredient){
+        $query = "SELECT * FROM `ingredient` WHERE `id`= ?;";
+        $sth = $dbh->prepare($query);
+        $request_succeeded = $sth->execute(array($id_ingredient));
+        if ($request_succeeded){
+            $nom_ingredient = $sth->fetch();
+            $sth->closeCursor();
+            if ($nom_ingredient === false){
+                return null;
+            }
+            return $nom_ingredient;
+        }
+        else{
+            return null;
+        }
+    }
+    public static function convert_nomIngredient_to_id($dbh,$nom_ingredient){
+        $query = "SELECT * FROM `ingredient` WHERE `nom`= ?;";
+        $sth = $dbh->prepare($query);
+        $request_succeeded = $sth->execute(array($nom_ingredient));
+        if ($request_succeeded){
+            $id_ingredient = $sth->fetch();
+            $sth->closeCursor();
+            if ($id_ingredient === false){
+                return null;
+            }
+            return $id_ingredient;
+        }
+        else{
+            return null;
+        }
+    }
     public static function liste_ingredients($dbh){
         $query = "SELECT * FROM `ingredient`;";
         $sth = $dbh->prepare($query);
@@ -16,6 +48,9 @@ class Ingredient{
             if ($li_ingredient === false){
                 return null;
             }
+            foreach($li_ingredient as $ingredient){
+                $ingredient['nom_ingredient'] = convert_id_to_nomIngredient($dbh,$ingredient['id']);
+            }
             return $li_ingredient;
         }
         else{
@@ -23,16 +58,17 @@ class Ingredient{
         }
     }
     
-    public static function getIngredient($dbh,$nom){
-        $query = "SELECT * FROM `ingredient` WHERE nom = ?;";
+    public static function getIngredient($dbh,$id){
+        $query = "SELECT * FROM `ingredient` WHERE id = ?;";
         $sth = $dbh->prepare($query);
-        $request_succeeded = $sth->execute(array($nom));
+        $request_succeeded = $sth->execute(array($id));
         if ($request_succeeded){
             $ingredient = $sth->fetch();
             $sth->closeCursor();
             if ($ingredient === false){
                 return null;
             }
+            $ingredient['nom_ingredient'] = convert_id_to_nomIngredient($dbh,$ingredient['id']);
             return $ingredient;
         }
         else{
@@ -41,15 +77,16 @@ class Ingredient{
     }
     
     public static function insererIngredientRecette($dbh,$nom_ingredient,$id_recette,$quantite,$unite) {
-        $sth = $dbh->prepare("INSERT INTO `ingredient_recette` (`nom_ingredient`,`id_recette`,`quantite`,`unite`) VALUES(?,?,?,?)");
-        return $sth->execute(array($nom_ingredient,$id_recette,$quantite,$unite));
+        $id_ingredient = convert_nomIngredient_to_id($dbh,$nom_ingredient);
+        $sth = $dbh->prepare("INSERT INTO `ingredient_recette` (`id_ingredient`,`id_recette`,`quantite`,`unite`) VALUES(?,?,?,?)");
+        return $sth->execute(array($id_ingredient,$id_recette,$quantite,$unite));
     }
     
-    public static function insererIngredient($dbh, $nom) {
-        if (Ingredient::getIngredient($dbh,$nom) !== null){
+    public static function nouvelIngredient($dbh, $nom) {
+        if (Ingredient::convert_nomIngredient_to_id($dbh,$nom) !== null){
             return false;
         }
-        $sth = $dbh->prepare("INSERT INTO `ingredient` (`nom`) VALUES(?)");
+        $sth = $dbh->prepare("INSERT INTO `ingredient` (`id`,`nom`) VALUES(null,?)");
         return $sth->execute(array($nom));
     }
 }
