ad5aee9d65a3df1d65185603c375779df5ed5226
mathieugenova <mathieugenova78@gmail.com>
2021-03-15 01:58:04 +0100

    register

--- content/content_Profil.php
+++ content/content_Profil.php
@@ -33,15 +33,16 @@ function getContent($args) {
         
 
     </div>
+    
+
+    <div class="jumbotron text-center">
+        <h2 style="font-size: 60px; color: #244b20;">Mes Recettes</h2>
     <?php
     foreach($liste_recettes as $recette){
         echo '<div class="row shadow p-3 mb-5 bg-white rounded align-items-center" style="margin: 3%">
                 <div class="col-4 text-center">
                     <h3>'.htmlspecialchars($recette["nom_plat"]).'</h3>
                     <p> <a class="bolien" href="index.php?page=Recette&recette='.htmlspecialchars($recette["id"]).'">Voir plus</a> </p>
-                </div>
-                <div class="col-4">
-                    <p>'.htmlspecialchars($recette["description"]).'</p>
                 </div>
                 <div class="col-4">
                     <a href="index.php?page=Recette&recette='.$recette["id"].'">
@@ -52,7 +53,7 @@ function getContent($args) {
     }
     
     ?>
-   
+    </div>
 
 
 
